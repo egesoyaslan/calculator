2d48b28710bb83dd4f482aaf12abbb644c23c97c
egesoyaslan <ege.soyaslan@yahoo.com>
2023-12-24 12:19:48 +0300

    Edit index.html and script.js
    Added a loop that enables the user to make multiple operations with one
    line.

--- script.js
+++ script.js
@@ -25,22 +25,30 @@ function operate(a, b, c) {
     if (b === `÷`) return divide(a, c);
 }
 
-function assign(string) {
-    const array = string.split(` `);
-    num1 = array[0];
-    operator = array[1];
-    num2 = array[2];
+function assign(givenArray) {
+    num1 = givenArray[0];
+    operator = givenArray[1];
+    num2 = givenArray[2];
 }
 
 let displayValue = ``;
+const display = document.querySelector(`.display`);
 
 const buttons = document.querySelector(`.buttons`);
 buttons.addEventListener(`click`, (e) => {
     const target = (e.target).textContent;
 
-    if (target === `=`) {
-        assign(displayValue);
-        displayValue = operate(num1, operator, num2);
+    if (target === `=`) { 
+        let array = [];
+        array = displayValue.split(` `);
+
+        assign(array);
+        while (array.length > 1) {
+            array[0] = operate(num1, operator, num2);
+            array.splice(1, 2);
+            assign(array);
+        }
+        displayValue = num1;
     }
 
     else if (target === `AC`) {
@@ -48,4 +56,6 @@ buttons.addEventListener(`click`, (e) => {
     }
 
     else displayValue += target;
+
+    display.textContent = displayValue;
 });
